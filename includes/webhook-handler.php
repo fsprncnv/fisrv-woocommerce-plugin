70b5e7714587dd25c58809498b29ae319260c34d
prncn <princen@outlook.de>
2024-05-23 10:37:07 +0200

    Store event log of webhook as orders meta

--- includes/webhook-handler.php
+++ includes/webhook-handler.php
@@ -2,50 +2,108 @@
 
 class WebhookHandler
 {
-    public static string $webhook_endpoint = '/wp-json/fiserv/v1/api';
+    public static string $webhook_endpoint = '/fiserv_woocommerce_plugin/v1';
 
-    private array $event_log = [];
+    private static array $event_log = [];
+
+    public static $instance;
+
+    public static function get()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
 
     public function __construct()
     {
+        self::$instance = $this;
         add_action('rest_api_init', [$this, 'register_post_route']);
         add_action('rest_api_init', [$this, 'register_get_route']);
     }
 
-    function callback_hook(WP_REST_Request $request)
+    /**
+     * Receive event from Fiserv checkout solution
+     * 
+     * @param WP_REST_Request $request Event data
+     * @return WP_REST_Response Reponse acknowledging sent data
+     * @return WP_Error 403 Code if request has failed
+     * 
+     * @todo Error handling when handling response object !!
+     */
+    public function consume_event(WP_REST_Request $request)
     {
         $request_body = $request->get_body();
 
         try {
+            array_push(self::$event_log, "Message at " . time());
             $json = json_decode($request_body);
-            array_push($this->event_log, "Message");
+            $current_instance = spl_object_hash($this);
+            $log_size = count(self::$event_log);
 
             $response = new WP_REST_Response($json);
             $response->set_status(200);
+
+            $order_id = intval($json['orderId']);
+            self::store_data_into_orders_meta($order_id, $request_body);
+
             return $response;
-        } catch (Exception $th) {
-            return new WP_Error('Something went wrong', $th->getMessage(), array('status' => 403));
+        } catch (Exception $e) {
+            return new WP_Error('Something went wrong', $e->getMessage(), array('status' => 403));
         }
     }
 
-    function register_post_route()
+    /**
+     * Register POST route at /wp-json/fiserv_woocommerce_plugin/v1/api.
+     * Receive from events from Fiserv checkout solution
+     */
+    public function register_post_route()
     {
-        register_rest_route('fiserv/v1', '/api', [
+        register_rest_route(self::$webhook_endpoint, '/events', [
             'methods' => 'POST',
-            'callback' => [$this, 'callback_hook']
+            'callback' => [$this, 'consume_event']
         ]);
     }
 
-    function serve_event_logs()
+    /**
+     * Callback method for GET route.
+     * Response data is list of saved events.
+     * 
+     * @return WP_REST_Response Response data
+     */
+    public static function serve_event_logs()
     {
-        return new WP_REST_Response($this->event_log);
+        return new WP_REST_Response(self::$cur);
     }
 
-    function register_get_route()
+    /**
+     * Register GET route at /wp-json/fiserv_woocommerce_plugin/v1/api.
+     * Display all entries of event log.
+     */
+    public function register_get_route()
     {
-        register_rest_route('fiserv/v1', '/api', [
+        register_rest_route(self::$webhook_endpoint, '/events', [
             'methods' => 'GET',
             'callback' => [$this, 'serve_event_logs']
         ]);
+    }
+
+    /**
+     * Store event log data into Wordpress table as order
+     * meta data.
+     */
+    private static function store_data_into_orders_meta(int $order_id, string $event_data): void
+    {
+        global $wpdb;
+
+        $data = [
+            'order_id' => $order_id,
+            'meta_key' => '_fiserv_plugin_webhook_event',
+            'meta_value' => $event_data,
+        ];
+
+        $format = ['%d', '%s', '%s'];
+        $insert = $wpdb->insert('wp_wc_orders_meta', $data, $format);
     }
 }
