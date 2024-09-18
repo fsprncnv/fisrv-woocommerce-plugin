6f5f6a8e97a856edaaa22c6ad24914f4ba2f488f
prncn <princen@outlook.de>
2024-09-18 12:27:04 +0200

    Change Playwright CI configuration

--- tests/tests/store.spec.ts
+++ tests/tests/store.spec.ts
@@ -1,9 +1,7 @@
 import { test, expect, Page } from '@playwright/test';
 
-const STORE_HOST = 'http://fisrv-plugin-dev.com/';
-
 test.beforeEach(async ({ page }) => {
-  await page.goto(STORE_HOST + '/?add-to-cart=12');
+  await page.goto('/?add-to-cart=12');
 });
 
 // ADMIN ENV CONFIG
@@ -22,38 +20,91 @@ test('Setup plugin and successful API health check (success flow)', async ({
   page,
 }) => {});
 
-test('Enable generic option and check specific method should be disabled', async ({
+test('Exclusively on enable either generic method or one or more specific method', async ({
   page,
 }) => {});
 
-// ... enable specific (apple pay) and disable generic
-
-
-// WP WEB SHOP 
+// WP WEB SHOP
 
 test('Web shop data (localization based on WP-admin, cart items) are properly passed to redirect page', async ({
-  // wp-admin set global language to german
   page,
-}) => {});
+}) => {
+  // wp-admin set global language to german
+});
 
 test('Failed order due to payment validation failure', async ({ page }) => {});
 
-test('Successful order', async ({ page }) => {
-  await page.goto(STORE_HOST + '/checkout');
+test.describe('Successful order and partial refund', () => {
+  let page: Page;
+  let orderNumber: string;
 
-  await fillOutWCCheckoutForm(page);
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
 
-  await page.locator('#payment_method_fisrv-gateway-generic').click();
+  test('01. Fill cart and fill in billing info in guest session', async ({
+    page,
+  }) => {
+    await page.goto('/checkout');
+
+    await fillOutBillingFormOnStore(page);
+  });
+
+  test('02. Select payment, successful redirect to hosted checkout page and redirect back to thank you page', async ({
+    page,
+  }) => {
+    orderNumber = await createSuccessfulOrder(page);
+  });
+
+  test('04. Order notes created', async ({ page }) => {
+    await page.goto(
+      `/wp-admin/admin.php?page=wc-orders&action=edit&id=${orderNumber}`
+    );
+
+    await expect(
+      page.getByText(/Fiserv checkout link, [A-Za-z]+$/i)
+    ).toBeVisible();
+  });
+
+  test('05. Refund order', async ({ page }) => {});
+});
+
+test('Autocomplete sets order status to complete', async ({ page }) => {});
+
+test('Checkout details report box on order page', async ({ page }) => {});
+
+test('Change generic payment icon and revert back', async ({ page }) => {});
+
+async function fillOutBillingFormOnStore(page: Page) {
+  await page.locator('#billing_first_name').fill('Frodo');
+  await page.locator('#billing_last_name').fill('Franklin');
+  await page.locator('#billing_address_1').fill('Street');
+  await page.locator('#billing_postcode').fill('12345');
+  await page.locator('#billing_city').fill('City');
+  await page.locator('#billing_phone').fill('0123456');
+  await page.locator('#billing_email').fill('dev@mail-playwright-12345.io');
+}
+
+async function fillOutPaymentFormOnHostedPaymentPage(
+  page: Page,
+  ccNumber: string
+) {
+  await page.locator("input[name='cardNumber']").fill(ccNumber);
+  await page.locator("input[name='expiryDate']").fill('0129');
+  await page.locator("input[name='cvvNumber']").fill('123');
+  await page.locator("input[name='ccname']").fill('Frodo Franklin');
+}
+
+async function createSuccessfulOrder(page: Page): Promise<any> {
+  if (await page.locator('#payment_method_fisrv-gateway-generic').isVisible()) {
+    await page.locator('#payment_method_fisrv-gateway-generic').click();
+  }
 
   await page.locator('#place_order').click();
 
   await page.waitForURL('https://ci.checkout-lane.com/#/?checkoutId=**');
 
-  // await expect(
-  //   page.getByRole('heading', { name: 'Überprüfen und Bezahlen' })
-  // ).toBeVisible();
-
-  await fillOutHostedPaymentPageForm(page, '5424180279791732');
+  await fillOutPaymentFormOnHostedPaymentPage(page, '5424180279791732');
 
   await page.locator("[appdataautomationid='payment_button']").click();
 
@@ -64,29 +115,9 @@ test('Successful order', async ({ page }) => {
   await expect(
     page.locator("[data-block-name='woocommerce/order-confirmation-status']")
   ).toBeVisible();
-});
 
-test('Order notes created', async ({ page }) => {});
-
-// get full response report from fiserv server
-
-test('Autocomplete sets order status to complete', async ({ page }) => {});
-
-test('Refund order', async ({ page }) => {});
-
-async function fillOutWCCheckoutForm(page: Page) {
-  await page.locator('#billing_first_name').fill('Frodo');
-  await page.locator('#billing_last_name').fill('Franklin');
-  await page.locator('#billing_address_1').fill('Street');
-  await page.locator('#billing_postcode').fill('12345');
-  await page.locator('#billing_city').fill('City');
-  await page.locator('#billing_phone').fill('0123456');
-  await page.locator('#billing_email').fill('dev@mail-playwright-12345.io');
-}
-
-async function fillOutHostedPaymentPageForm(page: Page, ccNumber: string) {
-  await page.locator("input[name='cardNumber']").fill(ccNumber);
-  await page.locator("input[name='expiryDate']").fill('0129');
-  await page.locator("input[name='cvvNumber']").fill('123');
-  await page.locator("input[name='ccname']").fill('Frodo Franklin');
+  return page
+    .locator('.wc-block-order-confirmation-summary-list-item__value')
+    .first()
+    .innerHTML();
 }
