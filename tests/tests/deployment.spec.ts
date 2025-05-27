6743d744a6eec04ed5f7c07e43381f34b500582f
Marc Herrmann <info@marcherrmann.com>
2025-05-27 11:37:16 +0200

    Updated Test Case

--- tests/tests/deployment.spec.ts
+++ tests/tests/deployment.spec.ts
@@ -2,9 +2,9 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Sucessful Deployment", () => {
 
-  test("Verify Latest Deployment", async () => {
+  test("Verify Latest Deployment", async ({ page }) => {
 	  await expect(async () => {
-		  const response = await page.request.get('https://api.example.com');
+		  const response = await page.request.get(process.env.WP_URL);
 		  expect(response.status()).toBe(200);
 		  expect(response.headers()["x-commit"]).toContain(process.env.CI_COMMIT_SHA ?? "test");
 	  }).toPass({
