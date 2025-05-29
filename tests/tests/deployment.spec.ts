383d6b7969c33ea72b2bddbfdd80213c178d96bb
Marc Herrmann <info@marcherrmann.com>
2025-05-29 20:07:39 +0200

    Edit deployment.spec.ts

--- tests/tests/deployment.spec.ts
+++ tests/tests/deployment.spec.ts
@@ -5,6 +5,7 @@ test.describe("Sucessful Deployment", () => {
   test("Verify Latest Deployment", async ({ page }) => {
 	  await expect(async () => {
 		  const response = await page.request.get(process.env.WP_URL);
+		  console.log(response.headers()["x-commit-id"]);
 		  expect(response.status()).toBe(200);
 		  expect(response.headers()["x-commit-id"]).toContain(process.env.CI_COMMIT_SHORT_SHA ?? "test");
 	  }).toPass({
