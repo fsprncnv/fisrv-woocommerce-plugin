50b107c2b1af48a586bde2eb0980a9f08ed0385b
John Cutler <john.cutler@fiserv.com>
2026-03-04 14:12:48 +0000

    GWS-86604: Should have been iDEAL | Wero

--- fiserv-checkout-for-woocommerce/includes/class-wc-fiserv-gateway-ideal.php
+++ fiserv-checkout-for-woocommerce/includes/class-wc-fiserv-gateway-ideal.php
@@ -16,10 +16,16 @@ class WC_Fiserv_Gateway_Ideal extends WC_Fiserv_Payment_Gateway
         $this->selected_method = PreSelectedPaymentMethod::IDEAL;
         $this->id = Fisrv_Identifiers::GATEWAY_IDEAL->value;
 
-        $this->method_title = 'iDEAL';
-        $this->method_description = __('Pay with iDEAL', 'fiserv-checkout-for-woocommerce');
-        $this->title = 'iDEAL';
+        $this->method_title = 'iDEAL | Wero'; // This is the title shown in the admin settings and checkout page
+        $this->method_description = __('Pay with iDEAL | Wero', 'fiserv-checkout-for-woocommerce');// This is the description shown in the admin settings under the title
+        $this->title = 'iDEAL | Wero';
         $this->description = esc_html__('You will be redirected to an external checkout page.', 'fiserv-checkout-for-woocommerce');
+
+            // Test if constructor runs (WooCommerce log)
+            if (function_exists('wc_get_logger')) {
+                $logger = wc_get_logger();
+                $logger->info('WC_Fiserv_Gateway_Ideal constructor executed', array('source' => 'fiserv-ideal-test'));
+            }
 
         parent::__construct();
     }
